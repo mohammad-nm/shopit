d8e1d6e8fde0ee38312017588d4f75ba16987aff
Mohammad Nickmagham <mohammadnickmagham@gmail.com>
2024-12-07 00:08:11 +0330

    Update UI

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,11 +1,152 @@
 import Footer from "@/components/Footer";
 import Navbar from "@/components/Navbar";
+import Route from "@/components/Route";
 
 export default function Products() {
   return (
-    <div>
+    <div className="bg-[#fff] text-black">
       <Navbar />
-      <div>Products</div>
+      <Route />
+      <div className="flex max-w-[850px] mx-auto p-8" dir="rtl">
+        <div className="w-1/4 flex flex-col gap-8">
+          <div className="bg-[#f8f8f8] p-4 flex flex-col gap-2 text-sm font-semibold text-gray-500">
+            <div className="text-lg text-black mb-4">دسته بندی ها</div>
+            <div className="flex items-center justify-between gap-2">
+              <div>بدون دسته‌بندی</div>
+            </div>
+            <div className="flex items-center justify-between gap-2">
+              <div>آشپزخانه و باغبانی</div>
+              <div>{">"}</div>
+            </div>
+            <div className="flex items-center justify-between gap-2">
+              <div>پوشاک</div>
+              <div>{">"}</div>
+            </div>
+            <div className="flex items-center justify-between gap-2">
+              <div>جواهرات و ساعت</div>
+              <div>{">"}</div>
+            </div>
+            <div className="flex items-center justify-between gap-2">
+              <div>کتاب</div>
+            </div>
+            <div className="flex items-center justify-between gap-2">
+              <div>لوازم الکتریکی</div>
+              <div>{">"}</div>
+            </div>
+            <div className="flex items-center justify-between gap-2">
+              <div>مادر و فرزند</div>
+            </div>
+            <div className="flex items-center justify-between gap-2">
+              <div>موبایل و لوازم جانبی</div>
+              <div>{">"}</div>
+            </div>
+            <div className="flex items-center justify-between gap-2">
+              <div>ورزشی</div>
+              <div>{">"}</div>
+            </div>
+          </div>
+          <div className="bg-[#f8f8f8] p-4 flex flex-col gap-4">
+            {" "}
+            <div>
+              <div>فیلترهای فعال</div>
+              <div>فیلترز</div>
+            </div>
+            <div>
+              <div>فیلتر براساس برند</div>
+              <div>
+                <input
+                  type="text"
+                  placeholder="جستجوی برند"
+                  className="w-full border border-gray-300 rounded-md p-2"
+                />
+              </div>
+            </div>
+            <div>
+              <div className="flex items-center gap-2">
+                <input type="checkbox" />
+                <div>آدیداس</div>
+              </div>
+
+              <div className="flex items-center gap-2">
+                <input type="checkbox" />
+                <div>اپل</div>
+              </div>
+
+              <div className="flex items-center gap-2">
+                <input type="checkbox" />
+                <div>تسکو</div>
+              </div>
+
+              <div className="flex items-center gap-2">
+                <input type="checkbox" />
+                <div>سامسونگ</div>
+              </div>
+            </div>
+            <div>
+              <div>فیلتر براساس قیمت</div>
+              <div>
+                <input type="range" className="w-full" />
+              </div>
+            </div>
+            <div>
+              <button className="bg-[#fcb800] text-black px-4 py-2 rounded-md w-full hover:bg-[#fcb155] transition-all duration-300 text-sm font-semibold">
+                فیلتر
+              </button>
+            </div>
+            <div>
+              <div>فیلتر بر اساس رنگ</div>
+              <div className="flex items-center gap-2">
+                <input type="checkbox" />
+                <div>سفید</div>
+              </div>{" "}
+              <div className="flex items-center gap-2">
+                <input type="checkbox" />
+                <div>مشکی</div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="w-3/4 flex flex-col px-8">
+          <div className="mb-6">نتیجه مطالعه برای ""</div>
+          <div className="flex w-full justify-between bg-[#f5f5f5] p-4 items-center">
+            <div className="flex gap-4">
+              <div>نوع نمایش</div>
+              <div className="flex gap-2">
+                <div>icon</div>
+                <div>icon</div>
+              </div>
+            </div>
+            <div>
+              <select
+                name="sorting"
+                id="sorting"
+                className="w-72 border border-gray-300 px-3 h-9"
+              >
+                {" "}
+                <option value="ارتباط"> ارتباط</option>
+                <option value="ارتباط"> ارتباط</option>
+                <option value="ارتباط"> ارتباط</option>
+                <option value="ارتباط"> ارتباط</option>
+              </select>
+            </div>
+            <div>10 محصول یافت شد</div>
+          </div>
+          <div className="p-4 flex flex-wrap w-full">
+            <div className="w-1/4 hover:border border border-transparent hover:border-gray-400 p-2">
+              <div className="w-full mb-2">
+                <img
+                  src="https://mimwp.com/theme/martfury/demo8/wp-content/uploads/2019/08/18a.jpg"
+                  alt=""
+                />
+              </div>
+              <div className="text-blue-500 mb-2 font-semibold">
+                تلویزیون هوشمند سامسونگ
+              </div>
+              <div className="font-medium">750 تومن</div>
+            </div>
+          </div>
+        </div>
+      </div>
       <Footer />
     </div>
   );
--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,7 +1,112 @@
+"use client";
 import Footer from "@/components/Footer";
 import Navbar from "@/components/Navbar";
 import Route from "@/components/Route";
+import { useState } from "react";
 export default function MyAccount() {
+  const [loggedIn, setLoggedIn] = useState(false);
+  const [login, setLogin] = useState(true);
+  if (!loggedIn)
+    return (
+      <div className="bg-[#f1f1f1]">
+        <Navbar />
+        <Route />
+        <div
+          className="flex flex-col max-w-[850px] mx-auto p-10 text-black mb-20 items-center gap-20"
+          dir="rtl"
+        >
+          <div className="flex gap-12 font-semibold text-4xl">
+            <button
+              className={`${!login ? "text-gray-300" : ""}`}
+              onClick={() => setLogin(true)}
+            >
+              ورود
+            </button>
+            <button
+              className={`${login ? "text-gray-300" : ""}`}
+              onClick={() => setLogin(false)}
+            >
+              عضویت
+            </button>
+          </div>
+          <div className="flex flex-col w-96 bg-[#fff] rounded-md p-8">
+            {login ? (
+              <div>
+                <div className="font-semibold text-lg mb-10">
+                  وارد حساب کاربری خود شوید
+                </div>
+                <div className="gap-4 flex flex-col mb-6">
+                  <input
+                    type="text"
+                    className="w-full h-16 p-4 border-[#d9d9d9;
+] border "
+                    placeholder="آدرس ایمیل"
+                  />
+                  <input
+                    type="text"
+                    className="w-full h-16 p-4 border-[#d9d9d9;
+] border "
+                    placeholder="رمز عبور"
+                  />
+                </div>
+                <div className="flex items-center gap-2 mb-4 ">
+                  <input type="checkbox" />
+                  مرا به خاطر بسپار
+                </div>
+                <div className="text-blue-500 mb-8">
+                  رمز عبور خود را فراموش کرده اید؟
+                </div>
+                <button
+                  className="w-full bg-[#fcb800] h-14 font-semibold text-lg "
+                  onClick={() => setLoggedIn(true)}
+                >
+                  {" "}
+                  ورود
+                </button>
+              </div>
+            ) : (
+              <div>
+                <div className="font-semibold text-lg mb-10">ثبت نام</div>
+                <div className="gap-4 flex flex-col mb-6">
+                  <div>
+                    <input
+                      type="text"
+                      className="w-full h-16 p-4 border-[#d9d9d9;
+] border "
+                      placeholder="آدرس ایمیل"
+                    />
+                  </div>
+                  <div className="flex items-center">
+                    <input
+                      type="text"
+                      className="w-full h-16 p-4 border-[#d9d9d9;
+] border "
+                      placeholder="رمز عبور"
+                    />
+                    <div className="mr-[-30px]">icon</div>
+                  </div>
+                </div>
+                <div className="font-semibold text-gray-400 mb-10">
+                  اطلاعات شخصی شما برای پردازش سفارش شما استفاده می‌شود، و
+                  پشتیبانی از تجربه شما در این وبسایت، و برای اهداف دیگری که در
+                  سیاست حفظ حریم خصوصی توضیح داده شده است.
+                </div>
+                <div>
+                  <button
+                    className="w-full bg-[#fcb800] h-14 font-semibold text-lg"
+                    onClick={() => setLoggedIn(true)}
+                  >
+                    عضویت
+                  </button>
+                </div>
+              </div>
+            )}
+          </div>
+        </div>
+        <Footer />
+      </div>
+    );
+
   return (
     <div className="bg-[#fff] ">
       <Navbar />
@@ -19,20 +124,31 @@ export default function MyAccount() {
             </div>
           </div>
           <div className="flex flex-col gap-y-2 font-bold text-sm bg-[#f6f6f6] border border-[#d1d1d1] rounded-md w-full">
-            <div className="border-b border-[#d1d1d1] bg-[#fcb800] px-4 py-2">
+            <button className="border-b border-[#d1d1d1] bg-[#fcb800] px-4 py-2 text-start">
               پیشخوان
-            </div>
-            <div className="border-b border-[#d1d1d1] px-4 py-2">سفارش ها</div>
-            <div className="border-b border-[#d1d1d1] px-4 py-2">دانلود ها</div>
-            <div className="border-b border-[#d1d1d1] px-4 py-2">آدرس</div>
-            <div className="border-b border-[#d1d1d1] px-4 py-2">
+            </button>
+            <button className="border-b text-start border-[#d1d1d1] px-4 py-2">
+              سفارش ها
+            </button>
+            <button className="border-b text-start border-[#d1d1d1] px-4 py-2">
+              دانلود ها
+            </button>
+            <button className="border-b text-start border-[#d1d1d1] px-4 py-2">
+              آدرس
+            </button>
+            <button className="border-b text-start border-[#d1d1d1] px-4 py-2">
               جزئیات حساب
-            </div>
-            <div className="border-b border-[#d1d1d1] px-4 py-2">خروج</div>
+            </button>
+            <button
+              className="border-b border-[#d1d1d1] px-4 py-2 text-start"
+              onClick={() => setLoggedIn(false)}
+            >
+              خروج
+            </button>
           </div>
         </div>
-        <div className="w-3/4 p-20 flex flex-col gap-y-8">
-          <div>سلام mohammad.ro1383 (mohammad.ro1383 نیستید؟ خارج شوید)</div>
+        <div className="w-3/4 p-20 flex flex-col gap-y-8 font-semibold text-gray-400">
+          <div>سلام mohammad (mohammad نیستید؟ خارج شوید)</div>
           <div>
             از طریق پیشخوان حساب کاربری‌تان، می‌توانید سفارش‌های اخیرتان را
             مشاهده، آدرس‌های حمل و نقل و صورتحساب‌تان را مدیریت و جزییات حساب
