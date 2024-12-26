826c6ceac11da2d48ec319741141ad39c7f6c832
Mohammad Nickmagham <mohammadnickmagham@gmail.com>
2024-12-26 15:28:55 +0330

    testing suspense

--- app/products/components/SkeletonLoader.tsx
+++ app/products/components/SkeletonLoader.tsx
@@ -1,9 +1,9 @@
 export default function SkeletonLoader() {
   return (
-    <div className=" space-y-4 animate-bounce">
-      <div className="h-20 bg-gray-300 rounded"></div>
-      <div className="h-20 bg-gray-300 rounded"></div>
-      <div className="h-20 bg-gray-300 rounded"></div>
+    <div className=" space-y-4 animate-pulse transition-opacity">
+      <div className="h-32 bg-gray-300 rounded"></div>
+      <div className="h-32 bg-gray-300 rounded"></div>
+      <div className="h-32 bg-gray-300 rounded"></div>
     </div>
   );
 }
