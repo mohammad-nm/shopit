c8284078c9c3f874d105e06c7d9af7c264abc4ed
Mohammad Nickmagham <mohammadnickmagham@gmail.com>
2025-01-03 19:55:22 +0330

    product page responsived

--- app/products/[id]/components/AddToCartBtn.tsx
+++ app/products/[id]/components/AddToCartBtn.tsx
@@ -32,11 +32,12 @@ export default function AddToCartBtn({
     }
   };
   return (
-    <div onClick={handleCart}>
+    <div
+      onClick={handleCart}
+      className="w-full xl:w-fit items-center justify-center text-center p-6 text-lg font-semibold bg-[#fcb800] hover:bg-[#fcb155] transition-all duration-300 text-black rounded-md "
+    >
       {" "}
-      <button className="bg-[#fcb800] hover:bg-[#fcb155] transition-all duration-300 text-black px-4 py-2 rounded-md ">
-        افزودن به سبد خرید
-      </button>
+      <button>افزودن به سبد خرید</button>
     </div>
   );
 }
--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -5,14 +5,35 @@ import Route from "@/components/Route";
 import { useEffect, useState } from "react";
 import { useParams, useRouter } from "next/navigation";
 import { useSelector } from "react-redux";
-import Image from "next/image";
-import CompareSvg from "@/svg/compare.svg";
-import MinusSvg from "@/svg/minus.svg";
-import PlusSvg from "@/svg/plus.svg";
+
 import getProduct from "./tools/getProduct";
 import AddToWishlistBtn from "./components/AddToWishlistBtn";
 import cookies from "js-cookie";
 import AddToCartBtn from "./components/AddToCartBtn";
+import QuantityInput from "./components/QuantityInput";
+import CompareBtn from "./components/ComapreBtn";
+import ProductTitle from "./components/ProductTitle";
+import ProductImage from "./components/ProductImage";
+import ProductPrice from "./components/ProductPrice";
+import ProductOptions from "./components/ProductOptions";
+import SectionsBtn from "./components/SectionsBtn";
+import ProductId from "./components/ProductId";
+import ProductDescription from "./components/ProductDescription";
+import ProductInfo from "./components/ProductInfo";
+import ProductComments from "./components/ProductComments";
+interface Product {
+  title: "";
+  pictureUrls: string[];
+  options: [];
+  id: "";
+  categories: [];
+  tags: [];
+  available: false;
+  price: "";
+  description: "";
+  info: [];
+  comments: [];
+}
 
 export default function Product() {
   const router = useRouter();
@@ -30,7 +51,7 @@ export default function Product() {
 
   const [loading, setLoading] = useState(false);
   const [quantity, setQuantity] = useState(1);
-  const [product, setProduct] = useState({
+  const [product, setProduct] = useState<Product>({
     title: "",
     pictureUrls: [],
     options: [],
@@ -60,199 +81,56 @@ export default function Product() {
     <div className="bg-[#fff] text-black">
       <Navbar />
       <Route />
-      <div className="max-w-[850px] mx-auto mt-14 mb-20" dir="rtl">
-        <div className="flex justify-between items-center mb-10 border-b border-gray-300 pb-4">
-          <div className="text-xl font-bold">{product.title}</div>
-          <div className="text-2xl font-bold">share icons</div>
-        </div>
-        <div className="flex gap-4 mb-10">
-          <div className="w-1/2">
-            <img
-              src={product.pictureUrls[0]}
-              alt="product picture"
-              className="h-3/4 object-cover w-full"
-            />
-          </div>
+      <div className="w-full md:w-4/5 mx-auto mt-14 mb-20" dir="rtl">
+        <ProductTitle title={product.title} />
+        <div className="flex flex-col md:flex-row gap-4 mb-10">
+          <ProductImage image={product.pictureUrls[0]} />
 
-          <div className="w-1/2 flex flex-col gap-4">
-            <div className="flex justify-between items-center border-b border-gray-300 pb-4">
-              <div className="text-2xl font-bold">{product.price} تومان</div>
-              <div className="text-sm text-[#00a881]">
-                وضعیت: {product.available ? "موجود" : "ناموجود"}
-              </div>
-            </div>
-            <div className="border-b border-gray-300 pb-4">
-              <div className="text-xs flex flex-col gap-1 font-semibold text-gray-400">
-                {product.options.map((option: any, index: number) => (
-                  <div key={index}>⁕ {option.name}</div>
-                ))}
-              </div>
-            </div>
-            <div className="flex items-center gap-6 border-b border-gray-300 pb-4">
-              <div
-                className="flex items-center border border-gray-300 rounded-md"
-                dir="ltr"
-              >
-                <button
-                  className="cursor-pointer  p-3"
-                  onClick={() =>
-                    setQuantity(quantity - 1 < 1 ? 1 : quantity - 1)
-                  }
-                >
-                  <Image src={MinusSvg} alt="minus" width={15} height={15} />
-                </button>
-                <div>
-                  <input
-                    type="text"
-                    className="w-10 text-center border-none outline-none"
-                    placeholder="1"
-                    onChange={(e) => setQuantity(parseInt(e.target.value))}
-                    value={quantity ? quantity : 1}
-                  />
-                </div>
-                <button
-                  className="cursor-pointer p-3"
-                  onClick={() => setQuantity(quantity + 1)}
-                >
-                  <Image src={PlusSvg} alt="plus" width={15} height={15} />
-                </button>
-              </div>
+          <div className="w-full md:w-1/2 flex flex-col gap-4 p-8">
+            <ProductPrice price={product.price} available={product.available} />
+            <ProductOptions options={product.options as []} />
+            <div className="flex flex-col xl:flex-row  items-center gap-6 border-b border-gray-300 pb-4">
+              <QuantityInput
+                quantity={quantity as number}
+                setQuantity={setQuantity}
+              />
               <AddToCartBtn
                 productId={productId as string}
                 quantity={quantity as number}
               />
-              <div className="gap-8 flex items-center">
+              <div className="gap-20 p-4 flex items-center">
                 <AddToWishlistBtn
                   productId={productId as string}
                   token={token}
                 />
-                <button className="flex flex-col items-center">
-                  <Image
-                    src={CompareSvg}
-                    alt="compare"
-                    width={20}
-                    height={20}
-                  />
-                  <div className="text-xs">Compare</div>
-                </button>
+                <CompareBtn />
               </div>
             </div>
             {error && <div className="text-red-500">{error}</div>}
-            <div className="flex flex-col gap-1 text-sm">
-              <div>شناسه : {product.id}</div>
-              <div>دسته بندی ها: {product.categories.join(", ")}</div>
-              <div>برچسب ها: {product.tags.join(", ")}</div>
-            </div>
-          </div>
-        </div>
-        <div className="flex justify-center items-center border-b border-gray-300 gap-10">
-          <button
-            className={`text-xl font-bold hover:text-[#fcb800] transition-all duration-300  hover:border-b-[#fcb800] pb-4 border-b-2 border-b-transparent ${
-              description === "description"
-                ? "border-b-[#fcb800] text-[#fcb800] "
-                : ""
-            }`}
-            onClick={() => setDescription("description")}
-          >
-            توضیحات
-          </button>
-          <button
-            className={`text-xl font-bold hover:text-[#fcb800] transition-all duration-300  hover:border-b-[#fcb800] pb-4 border-b-2 border-b-transparent ${
-              description === "info" ? "border-b-[#fcb800] text-[#fcb800] " : ""
-            }`}
-            onClick={() => setDescription("info")}
-          >
-            مشخصات
-          </button>
-          <button
-            className={`text-xl font-bold hover:text-[#fcb800] transition-all duration-300  hover:border-b-[#fcb800] pb-4 border-b-2 border-b-transparent ${
-              description === "comments"
-                ? "border-b-[#fcb800] text-[#fcb800] "
-                : ""
-            }`}
-            onClick={() => setDescription("comments")}
-          >
-            نظرات
-          </button>
-        </div>
-        {description === "description" ? (
-          <div className="mt-10" dir="ltr">
-            <div className="text-xl font-bold mb-4">{product.description}</div>
-            <div className="text-sm text-gray-500 mb-10">
-              {product.description}
-            </div>
-            <img
-              src={product.pictureUrls[0]}
-              alt="product"
-              className="w-full"
+            <ProductId
+              categories={product.categories as []}
+              id={product.id}
+              tags={product.tags as []}
             />
           </div>
+        </div>
+        <SectionsBtn
+          description={description}
+          setDescription={setDescription}
+        />
+        {description === "description" ? (
+          <ProductDescription
+            description={product.description}
+            image={product.pictureUrls[0]}
+          />
         ) : description === "info" ? (
-          <div className="mt-10">
-            <div>
-              <div className="flex flex-col ">
-                {product.info.map((info: any) => (
-                  <div
-                    key={info.infoTitle}
-                    className="flex border border-gray-300 gap-10 "
-                    dir="ltr"
-                  >
-                    <div className="text-xl font-semibold w-1/6 bg-gray-200 p-4">
-                      {info.infoTitle}
-                    </div>
-                    <div className="text-sm text-gray-500 font-semibold p-4">
-                      {info.info}
-                    </div>
-                  </div>
-                ))}
-              </div>
-            </div>
-          </div>
+          <ProductInfo info={product.info as []} />
         ) : description === "comments" ? (
-          <div className="mt-14">
-            <div className="font-bold text-lg">
-              اولین نفری باشید که دیدگاهی را برای “{product.title}” ارسال می
-              کنید
-            </div>
-            <div className="mt-6">امتیاز شما به این محصول</div>
-
-            <div>
-              <input
-                type="text"
-                className="w-full h-32 border border-gray-300 p-4 mt-8 "
-              />
-              <div className="mt-[-80px] mr-6 font-semibold text-gray-400 ">
-                بررسی خود را اینحا بنویسید...
-              </div>
-            </div>
-            <div className="flex justify-between items-center mt-24">
-              <button className="bg-[#fcb800] hover:bg-[#fcb155] transition-all duration-300 text-black px-4 py-2 ">
-                ارسال دیدگاه
-              </button>
-            </div>
-            <div className="mt-10">
-              <div className="text-xl font-bold">دیدگاه ها</div>
-            </div>
-            <div className="mt-10" dir="ltr">
-              {product.comments.map((comment: any, index: number) => (
-                <div
-                  key={index}
-                  className="flex flex-col gap-2 p-4 bg-gray-100 rounded-md mb-4"
-                >
-                  <div className="text-2xl font-bold">
-                    {comment.commentedBy}{" "}
-                    <span className="text-gray-500 text-sm">
-                      ⁕{comment.date}
-                    </span>
-                  </div>
-                  <div className="text-md text-gray-500">{comment.comment}</div>
-                </div>
-              ))}
-            </div>
-          </div>
-        ) : (
-          <div></div>
-        )}
+          <ProductComments
+            comments={product.comments as []}
+            title={product.title}
+          />
+        ) : null}
       </div>
       <Footer />
     </div>
